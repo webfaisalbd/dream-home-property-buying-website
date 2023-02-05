669c0ae31a1f7f4c4ed1cb6b27c8e6744685efe9
webfaisalbd <web.faisal.bd@gmail.com>
2023-02-05 19:01:50 +0600

    appointment section js part added

--- app.js
+++ app.js
@@ -4,14 +4,105 @@ let peopleMore = document.getElementById('people_more');
 
 let numberOfPeople = 3;
 
-peopleMore.addEventListener('click', ()=> {
+// people more button 
+peopleMore.addEventListener('click', () => {
     numberOfPeople = numberOfPeople + 1;
     people.value = `${numberOfPeople} People`;
 })
 
-peopleLess.addEventListener('click', ()=> {
-    if(numberOfPeople >1){
-    numberOfPeople = numberOfPeople - 1;
-    people.value = `${numberOfPeople} People`;
+// people less button 
+peopleLess.addEventListener('click', () => {
+    if (numberOfPeople > 1) {
+        numberOfPeople = numberOfPeople - 1;
+        people.value = `${numberOfPeople} People`;
     }
+})
+
+
+// appointment change input
+let appointmentDate = document.getElementById('appointmentDate');
+let setDate = document.getElementById('setDate');
+
+setDate.addEventListener('change', () => {
+    let choosenData = setDate.value;
+
+    let dates = new Date(choosenData);
+
+    // day 
+    let week = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
+    let day = dates.getDay();
+    let dayName = week[day];
+
+
+    // month 
+    let month = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'July', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
+    let monthName = month[dates.getMonth()];
+
+    // date 
+    let date = dates.getDate();
+
+    // today date 
+    let todayDate = new Date();
+
+
+    if (dates > todayDate) {
+        appointmentDate.textContent = `${dayName}, ${date} ${monthName}`;
+    }
+    // console.log(dayName + ', ' + date + ' ' + monthName)
+})
+
+
+
+//  appointment  previous day 
+let people_previous = document.getElementById('people_previous');
+
+people_previous.addEventListener('click', () => {
+    let choosenData = setDate.value;
+    let update = new Date(choosenData);
+    let previousDate = update.getDate() - 1;
+
+    let week = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
+    let weekValue = update.getDay() - 1;
+    let previousDay;
+    if (weekValue == -1) {
+        previousDay = week[6];
+    } else {
+        previousDay = week[weekValue];
+    }
+    // console.log(previousDay);
+
+    let month = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'July', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
+    let previousMonth = month[update.getMonth()];
+
+    // console.log(previousDay, previousDate, previousMonth);
+    appointmentDate.textContent = `${previousDay}, ${previousDate} ${previousMonth}`;
+    
+})
+
+
+
+//  appointment  next day
+let people_next = document.getElementById('people_next');
+
+people_next.addEventListener('click', () => {
+    let choosenData = setDate.value;
+    let update = new Date(choosenData);
+    let nextDate = update.getDate() + 1;
+    
+    let week = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
+    let weekValue = update.getDay() + 1;
+    let nextDay;
+    if(weekValue == 7){
+        nextDay = week[0];
+    }
+    else {
+        nextDay = week[weekValue];
+    }
+    // console.log(nextDay);
+
+    let month = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'July', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
+    let nextMonth = month[update.getMonth()];
+
+    // console.log(nextDay, nextDate, nextMonth)
+    appointmentDate.textContent = `${nextDay}, ${nextDate} ${nextMonth}`;
 })
